9ba3e7bb450bbbdb5c305518d6885a540bc74074
Jesper Sandström <jespersandstrom@gmail.com>
2017-11-02 18:36:46 +0100

    Consolidate fields

--- src/graphql/GraphQLCV.ts
+++ src/graphql/GraphQLCV.ts
@@ -6,57 +6,63 @@ import {
   GraphQLString,
 } from 'graphql'
 
+const MutableItemFields = {
+  title: { type: GraphQLString },
+  description: { type: GraphQLString },
+  start: { type: GraphQLInt },
+  end: { type: GraphQLInt },
+}
+
 export const GraphQLCVItem = new GraphQLObjectType({
   name: 'CVItem',
   fields: {
-    title: { type: GraphQLString },
-    description: { type: GraphQLString },
-    start: { type: GraphQLInt },
-    end: { type: GraphQLInt },
+    ...MutableItemFields,
   },
 })
 
 export const GraphQLCVItemInput = new GraphQLInputObjectType ({
   name: 'CVItemInput',
   fields: {
-    title: { type: GraphQLString },
-    description: { type: GraphQLString },
-    start: { type: GraphQLInt },
-    end: { type: GraphQLInt },
+    ...MutableItemFields,
   },
 })
+
+const MutableCVSectionFields = {
+  title: { type: GraphQLString },
+  description: { type: GraphQLString },
+  items: { type: new GraphQLList(GraphQLCVItem) },
+}
 
 export const GraphQLCVSection = new GraphQLObjectType({
   name: 'CVSection',
   fields: {
-    title: { type: GraphQLString },
-    description: { type: GraphQLString },
-    items: { type: new GraphQLList(GraphQLCVItem) },
+    ...MutableCVSectionFields,
   },
 })
 
 export const GraphQLCVSectionInput = new GraphQLInputObjectType({
   name: 'CVSectionInput',
   fields: {
-    title: { type: GraphQLString },
-    description: { type: GraphQLString },
-    items: { type: new GraphQLList(GraphQLCVItemInput) },
+    ...MutableCVSectionFields,
   },
 })
+
+const MutableCVFields = {
+  text: { type: GraphQLString },
+  sections: { type: new GraphQLList(GraphQLCVSection) },
+}
 
 export const GraphQLCV = new GraphQLObjectType({
   name: 'CV',
   fields: {
     userId: { type: GraphQLString },
-    text: { type: GraphQLString },
-    sections: { type: new GraphQLList(GraphQLCVSection) },
+    ...MutableCVFields,
   },
 })
 
 export const GraphQLCVInput = new GraphQLInputObjectType({
   name: 'CVInput',
   fields:  {
-    text: { type: GraphQLString },
-    sections: { type: new GraphQLList(GraphQLCVSectionInput) },
+    ...MutableCVFields,
   },
 })
