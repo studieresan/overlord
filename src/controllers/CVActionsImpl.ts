e667608907d2d4bed1c4b3f5ab2ddfabe398f550
Jesper Sandström <jespersandstrom@gmail.com>
2017-11-16 19:22:04 +0100

    Return a default CV for users which have not yet created one

--- src/controllers/CVActionsImpl.ts
+++ src/controllers/CVActionsImpl.ts
@@ -1,12 +1,17 @@
 import DB from '../mongodb/CV'
 import { CVActions } from './CVActions'
-import { CV } from '../models'
+import { CV, createDefaultCV } from '../models'
 import { cast } from './util'
 
 export class CVActionsImpl implements CVActions {
 
   getCV(userId: string): Promise<CV> {
     return DB.findOne({ userId })
+      .then(doc => {
+        return doc === null
+          ? createDefaultCV(userId)
+          : doc
+      })
       .then(cast<CV>())
   }
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,10 @@
-import { CV, CVSection } from './CV'
+import { CV, CVSection, createDefaultCV } from './CV'
 import { User, MemberType } from './User'
 
 export {
   CV,
   CVSection,
+  createDefaultCV,
   User,
   MemberType,
 }
