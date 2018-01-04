4f529ca393f68cfc4075fb6b5aadc32648fa61a6
Jonathan Berglind <jonathan.berglind@gmail.com>
2018-01-04 18:39:16 +0100

    Add GraphQL type for events

---
+++ src/graphql/GraphQLEvent.ts
@@ -0,0 +1,31 @@
+import {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInputObjectType,
+  GraphQLEnumType,
+  GraphQLString,
+} from 'graphql'
+import * as models from './../models'
+
+const MutableEventFields = {
+  schedule: { type: GraphQLString },
+  privateDescription: { type: GraphQLString },
+  publicDescription: { type: GraphQLString },
+  date: { type: GraphQLString },
+  beforeSurveys: { type: new GraphQLList(GraphQLString) },
+  afterSurveys: { type: new GraphQLList(GraphQLString) },
+  pictures: { type: new GraphQLList(GraphQLString) },
+}
+
+export const EventType = new GraphQLObjectType({
+  name : 'Event',
+  fields : {
+    companyName: { type: GraphQLString },
+    ...MutableEventFields,
+  },
+})
+
+export const EventInputType = new GraphQLInputObjectType({
+  name : 'UserProfileInput',
+  fields : MutableEventFields,
+})
