a7d77ff25c24ece67a0143a77d0a21cd1775ac86
Jesper Sandström <jespersandstrom@gmail.com>
2018-01-05 21:45:31 +0100

    Allow multiple events per company and fix some types

--- src/mongodb/Event.ts
+++ src/mongodb/Event.ts
@@ -6,6 +6,7 @@ export type EventDocument = mongoose.Document & models.Event
 const eventSchema: mongoose.Schema = new mongoose.Schema({
   companyName: String,
   schedule: String,
+  location: String,
   privateDescription: String,
   publicDescription: String,
   date: Date,
