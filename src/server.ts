78d2792aab615f9f3db24741cf3a9589882bc3eb
Michel Tabari <tabari@kth.se>
2018-10-23 23:40:37 +0200

    Try to fix Safari issue

--- src/server.ts
+++ src/server.ts
@@ -89,6 +89,8 @@ app.use(session({
   saveUninitialized: false,
   cookie : {
     maxAge: 24 * 60 * 60 * 1000 * 14, // two weeks
+    httpOnly: true,
+    secure: true,
   },
   secret: process.env.SESSION_SECRET,
   store: new MongoStore({
