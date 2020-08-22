1673a47c2e2fc7ffb8eff7d90d7c65b20a97d84b
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2020-08-22 14:53:37 +0200

    Get company responsible from list of ids and year

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,7 @@
 import { CV, CVSection, createDefaultCV } from './CV'
 import { User, UserProfile, UserRole, Permission } from './User'
 import { Event } from './Event'
-import { Company } from './Company'
+import { Company, CompanyWithSeveralUsers, CompanyWithOneUser } from './Company'
 import { CompanySalesStatus } from './CompanySalesStatus'
 import { SalesComment } from './SalesComment'
 import { CompanyContact } from './CompanyContact'
@@ -16,6 +16,8 @@ export {
   Permission,
   Event,
   Company,
+  CompanyWithSeveralUsers,
+  CompanyWithOneUser,
   CompanySalesStatus,
   SalesComment,
   CompanyContact,
--- src/mongodb/Company.ts
+++ src/mongodb/Company.ts
@@ -1,13 +1,13 @@
 import * as mongoose from 'mongoose'
 import * as models from '../models'
 
-export type CompanyDocument = mongoose.Document & models.Company
+export type CompanyDocument = mongoose.Document & models.CompanyWithSeveralUsers
 
 const companySchema: mongoose.Schema = new mongoose.Schema({
   name: String,
   amount: Number,
   status: {type: mongoose.Schema.Types.ObjectId, ref: 'CompanySalesStatus'},
-  responsibleUser: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  responsibleUsers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
 }, { timestamps: true })
 
 export const Company = mongoose.model<CompanyDocument>('Company', companySchema)
