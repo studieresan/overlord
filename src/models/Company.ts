a677fc1e474ea0d208e28afb06a3a57b765bc846
Anton Stagge <antonstagge95@gmail.com>
2020-08-26 13:08:37 +0200

    Add default status for new year

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -1,33 +1,14 @@
 import { CompanySalesStatus, User } from '.'
 
-  export interface Company {
-    readonly id: string
-    name: string
-    years: CompanyYear[]
-  }
+export interface Company {
+  readonly id: string
+  name: string
+  years: CompanyYear[]
+}
 
-  export interface CompanyYear {
-    year: number,
-    amount: number,
-    status: CompanySalesStatus,
-    responsibleUser: User,
-  }
-
-  // export interface CompanyWithSeveralUsers extends Company {
-  //     responsibleUsers: User[]
-  //   }
-
-  // export interface CompanyWithOneUser extends Company {
-  //   responsibleUser: User
-  // }
-
-  // export function companyFactory(companySeveral: CompanyWithSeveralUsers): CompanyWithOneUser {
-  //   const companyOne: CompanyWithOneUser = {
-  //     id: companySeveral.id,
-  //     name: companySeveral.name,
-  //     amount: companySeveral.amount,
-  //     status: companySeveral.status,
-  //     responsibleUser: companySeveral.responsibleUsers[0],
-  //   }
-  //   return companyOne
-  // }
+export interface CompanyYear {
+  year: number
+  amount: number
+  status: CompanySalesStatus
+  responsibleUser: User
+}
