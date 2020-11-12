270df4226461076cc8b2ec8883eadc19c060b3ec
Glenn Olsson <glennholsson@gmail.com>
2020-11-12 10:38:53 +0100

    Update event model in models

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -2,18 +2,14 @@ import { Company, User } from '.'
 
 export interface Event {
   readonly id: string
-  readonly companyName: string
   readonly company: Company
   readonly responsible: User
-  readonly schedule: string
   readonly location: string
   readonly privateDescription: string
   readonly publicDescription: string
   readonly date: Date
-  readonly beforeSurveys: string[]
-  readonly afterSurveys: string[]
+  readonly beforeSurvey: string
+  readonly afterSurvey: string
   readonly pictures: string[]
   readonly published: boolean
-  readonly checkedInUsers: User[]
-  notCheckedInUsers: User[]
 }
