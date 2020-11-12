15e66eee19af4867cf8bed68300ca8fab69a6032
Glenn Olsson <glennholsson@gmail.com>
2020-11-12 12:29:36 +0100

    Fix space indentation

--- src/mongodb/Event.ts
+++ src/mongodb/Event.ts
@@ -4,21 +4,21 @@ import * as models from '../models'
 export type EventDocument = mongoose.Document & models.Event
 
 const eventSchema: mongoose.Schema = new mongoose.Schema({
-		company: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
-		responsible: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-		location: String,
-		privateDescription: String,
-		publicDescription: String,
-		date: Date,
-		studsYear: Number,
-		published: {
-			type: Boolean,
-			default: false,
-		},
-		pictures: [String],
-		beforeSurvey: String,
-		afterSurvey: String,
-	}
+        company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+        responsible: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        location: String,
+        privateDescription: String,
+        publicDescription: String,
+        date: Date,
+        studsYear: Number,
+        published: {
+            type: Boolean,
+            default: false,
+        },
+        pictures: [String],
+        beforeSurvey: String,
+        afterSurvey: String,
+    }
 , { timestamps: true })
 
 export const Event =
