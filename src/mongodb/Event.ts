95dfb246b5ec50af3bd63502a0c3280930e39c2f
Glenn Olsson <glennholsson@gmail.com>
2020-11-12 10:38:19 +0100

    Change Event model in mongo

--- src/mongodb/Event.ts
+++ src/mongodb/Event.ts
@@ -4,24 +4,22 @@ import * as models from '../models'
 export type EventDocument = mongoose.Document & models.Event
 
 const eventSchema: mongoose.Schema = new mongoose.Schema({
-  companyName: String,
-  schedule: String,
-  location: String,
-  privateDescription: String,
-  publicDescription: String,
-  date: Date,
-  responsible: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  beforeSurveys: [String],
-  afterSurveys: [String],
-  pictures: [String],
-  company: {type: mongoose.Schema.Types.ObjectId, ref: 'Company'},
-  checkedInUsers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-  notCheckedInUsers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-  published: {
-    type: Boolean,
-    default: false,
-  },
-}, { timestamps: true })
+		company: { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
+		responsible: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+		location: String,
+		privateDescription: String,
+		publicDescription: String,
+		date: Date,
+		studsYear: Number,
+		published: {
+			type: Boolean,
+			default: false,
+		},
+		pictures: [String],
+		beforeSurvey: String,
+		afterSurvey: String,
+	}
+, { timestamps: true })
 
 export const Event =
   mongoose.model<EventDocument>('Event', eventSchema)
