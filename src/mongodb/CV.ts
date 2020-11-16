4cd04a0e165c2101c4b9905450d641df064dffb6
Glenn Olsson <glennholsson@gmail.com>
2020-11-16 14:04:02 +0100

    Update CV and User mongodb models

--- src/mongodb/CV.ts
+++ src/mongodb/CV.ts
@@ -5,7 +5,7 @@ export type CVDocument = mongoose.Document & models.CV
 
 const cvSchema: mongoose.Schema = new mongoose.Schema({
   userId: { type: String, unique: true },
-  text: String,
+  resumeEmail: String,
   sections: { type: [] },
 }, { timestamps: true })
 
