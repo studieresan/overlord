783f202f9a9e016011348b1ebd231adcfa5a0bac
Fredrik <fredrik.norlin@hotmail.se>
2021-02-28 22:38:15 +0100

    Add jest generates coverage report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ["/node_modules/", "/__tests__/utillity/"],
+  collectCoverage: true,
+  coverageReporters: ["html"]
 };
