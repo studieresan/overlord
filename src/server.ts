2d45e45bffb76bd84850965ea100b89e719ae451
Artin Mirzaian <mirzaian@kth.se>
2021-12-26 13:57:09 +0100

    trying to fix cors error

--- src/server.ts
+++ src/server.ts
@@ -82,8 +82,10 @@ app.use(function(req, res, next) {
     )
   }
   // Allow preflight
-  if (req.method === 'OPTIONS')
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Origin', origin)
     return res.end()
+  }
   next()
 })
 
