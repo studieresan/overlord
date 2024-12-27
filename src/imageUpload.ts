9a011dfa640016122ac42d53d3b0f0d34bc87827
viktorrn <viktorronnbackanyback@gmail.com>
2024-12-27 22:59:42 +0100

    fixed s3 image upload

--- src/imageUpload.ts
+++ src/imageUpload.ts
@@ -1,44 +1,52 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+// import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const s3Client = new S3({
+  apiVersion: '2006-03-01',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+  },
+  region: process.env.AWS_REGION ?? "",
+});
 
 export async function signedUploadRequest(req: any, res: any) {
-  const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
-  });
-
   const fileName = req.query['file-name']
   const fileType = req.query['file-type']
-  console.log("req.query: ", fileName, fileType)
-  const BUCKET = process.env.S3_BUCKET
-  const REGION = process.env.AWS_REGION
-  const key = `uploads/${Date.now()}/${fileName}`
-  console.log("AWS_REGION:", process.env.AWS_REGION);
-  console.log("S3_BUCKET:", process.env.S3_BUCKET);
-  console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
-  console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY);
+  console.log("fileName: ", fileName)
+  console.log("fileType: ", fileType)
 
+  if (!fileName || !fileType) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ error: "Missing required parameters" }));
+    res.end();
+    return;
+  }
 
-  const command = new PutObjectCommand({
-    Bucket: BUCKET,
+  const key = `${Date.now()}-${fileName}`
+
+  const params = {
+    Bucket: process.env.S3_BUCKET ?? "",
     Key: key,
     ContentType: fileType,
-    // ACL: 'public-read'
-  });
-
+  };
   try {
-    const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 60 });
+    const signedUrl = await getSignedUrl(s3Client, new PutObjectCommand(params), { expiresIn: 60 });
 
     const returnData = {
       signedRequest: signedUrl,
-      url: `https://${BUCKET}.s3.${REGION}.amazonaws.com/${key}`,
+      url: `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`,
     }
 
+
+    console.log("returnData: ", returnData)
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(returnData))
     res.end()
   } catch (err) {
     console.log(err)
-    res.end()
+    res.end(err)
   }
 }
 
